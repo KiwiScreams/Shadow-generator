7ce0d5ada75964488074080227004644e8483598
KiwiScreams <screamskiwi3@gmail.com>
2024-09-09 18:46:34 +0400

    shadow generator func

---
+++ script/app.js
@@ -0,0 +1,22 @@
+const box = document.querySelector('.box');
+const horizontalInput = document.getElementById('horizontal');
+const verticalInput = document.getElementById('vertical');
+const blurInput = document.getElementById('blur');
+const spreadInput = document.getElementById('spread');
+const colorInput = document.getElementById('color');
+
+horizontalInput.addEventListener('input', updateShadow);
+verticalInput.addEventListener('input', updateShadow);
+blurInput.addEventListener('input', updateShadow);
+spreadInput.addEventListener('input', updateShadow);
+colorInput.addEventListener('input', updateShadow);
+
+function updateShadow() {
+    const horizontal = parseInt(horizontalInput.value);
+    const vertical = parseInt(verticalInput.value);
+    const blur = parseInt(blurInput.value);
+    const spread = parseInt(spreadInput.value);
+    const color = colorInput.value;
+
+    box.style.boxShadow = `${horizontal}px ${vertical}px ${blur}px ${spread}px ${color}`;
+}
